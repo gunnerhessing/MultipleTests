ce2dd8a7c9819b5a378fb565ac3357ded088ff94
gunnerhessing <gunner.hessing3461@stu.mtec.edu>
2023-11-29 12:38:09 -0700

    CleaningCommit

--- test/pageobjects/secure.page.js
+++ test/pageobjects/secure.page.js
@@ -5,9 +5,7 @@ const Page = require('./page');
  * sub page containing specific selectors and methods for a specific page
  */
 class SecurePage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+  
 
     get errorButton () {
         return $('.error-button');

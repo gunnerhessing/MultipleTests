fb87400d5144a9e8de4048ea907fdbdfab844a4d
gunnerhessing <gunner.hessing3461@stu.mtec.edu>
2023-11-30 13:23:38 -0700

    addtoCart

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -25,7 +25,7 @@ class LoginPage extends Page {
     }
 
 
-    /**
+    /** 
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password 
      */
